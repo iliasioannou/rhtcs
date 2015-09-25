ec7b739bb060f7733e0ad295614b5edcae55166f
lunanova <lunanova@planetek.it>
2015-09-25 11:13:25 +0200

    [etl_modis] Updated Unit Test Deleted copy of MODIS_L2_LAC.nc product file from resources folder

--- ESB/etl_modis/src/test/java/it/planetek/rheticus/mule/components/MODISMetadataReaderComponentTest.java
+++ ESB/etl_modis/src/test/java/it/planetek/rheticus/mule/components/MODISMetadataReaderComponentTest.java
@@ -3,20 +3,24 @@ package it.planetek.rheticus.mule.components;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.Map;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import it.planetek.rheticus.mule.components.MODISMetadataReaderComponent;
-
 public class MODISMetadataReaderComponentTest {
 
-	MODISMetadataReaderComponent modis = null;
+	private MODISMetadataReaderComponent modis = null;
+	private File srcFile = new File("src/test/resources/product/MODIS_L2_LAC.nc");
+	private File destFile = new File("src/test/resources/MODIS_L2_LAC.nc");
+
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -29,6 +33,8 @@ public class MODISMetadataReaderComponentTest {
 	@Before
 	public void setUp() throws Exception {
 		modis = new MODISMetadataReaderComponent();
+
+		FileUtils.copyFile(srcFile, destFile);
 	}
 
 	@After
@@ -38,14 +44,26 @@ public class MODISMetadataReaderComponentTest {
 	@Test
 	public final void testReadMetadata() {
 		try {
+			System.out.println("[testReadMetadata] Start ...\n");
+			
 			@SuppressWarnings("unchecked")
-			Map<String, Object> metadata = (Map<String, Object>) modis.readMetadata("src/test/resources/MODIS_L2_LAC.nc");
+			Map<String, Object> metadata = (Map<String, Object>) modis.readMetadata(destFile.getPath());
 
 			assertNotNull(metadata);
+			System.out.println("Metadata is VALID!\n");	
+
 			assertTrue(metadata.size()>0);
+			System.out.println("Metadata size: "+metadata.size()+"\n");
+
+			for (Map.Entry<String, Object> entry : metadata.entrySet()) {
+				System.out.println("KEY  : \""+entry.getKey()+"\"");
+				System.out.println("VALUE: \""+entry.getValue().toString()+"\"\n");
+			}
 
 		} catch (IOException e) {
 			System.out.println("[testReadMetadata] IOException "+e.getMessage());
+		} finally {
+			System.out.println("[testReadMetadata] End ...");
 		}
 	}
 
