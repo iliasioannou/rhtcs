968622b130e95385374e58787ca46c65e264d0c6
Stefano Sportelli <sportelli@planetek.it>
2015-10-12 15:49:59 +0200

    add Rheticus GUI

---
+++ rheticus-gui/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name workspacePilotApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the workspacePilotApp
+ */
+angular.module('workspacePilotApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
