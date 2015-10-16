740aadd46799afd7ff0757bcc0643a99b266b7e7
marco fago <fago@planetek.it>
2015-10-16 16:54:11 +0200

    Added wwms

--- rheticus-gui/app/scripts/controllers/main.js
+++ rheticus-gui/app/scripts/controllers/main.js
@@ -19,19 +19,49 @@ angular.module('workspacePilotApp')
 				{name : 'DS5', selected: false},
 				{name : 'DS6', selected: false},
 			],
+			selectableDates: [
+				"dl20110521",
+				"dl20110724",
+				"dl20110926",
+				"dl20120217",
+				"dl20120507",
+				"dl20120908",
+				"dl20121026",
+				"dl20121217",
+				"dl20130130",
+				"dl20130303",
+				"dl20130506",
+				"dl20130725",
+				"dl20130927",
+				"dl20131114",
+				"dl20131216",
+				"dl20140121",
+				"dl20140310",
+				"dl20140423",
+				"dl20140610",
+				"dl20140712",
+				"dl20140813",
+				"dl20140914",
+				"dl20141101",
+				"dl20141219",
+				"dl20150124",
+				"dl20150225",
+				"dl20150329",
+				"dl20150414",
+				"dl20150516"
+			]
+	  });
+	  angular.extend($scope, {
 			dsModel : [],
 			selectedDatatsets : [],
-			timeModel : "0;50",
+			timeModel : "1",
 			timeOptions : {       
 				from: 1,
-				to: 100,
+				to: $scope.selectableDates.length-1,
 				step: 1,
-				dimension: " days ago",
-				scale: [0, '|', 50, '|' , 100],
-				skin: 'plastic',
-				      
+				modelLabels: $scope.selectableDates,
+				dimension: "",
 			},
-			
 			speedModel : "10",
 			speedOptions : {       
 				from: 1,
@@ -60,9 +90,9 @@ angular.module('workspacePilotApp')
 					default: {"background-color": "white"},
 					after: {"background-color": "green"},
 					pointer: {"background-color": "red"}   
-				}  
-			}
-	  });
+				}
+			}				
+		});
   }])
   .controller('MainCtrl', ['$scope',function ($scope) {
     
@@ -73,33 +103,33 @@ angular.module('workspacePilotApp')
 			zoom: 14
 		},
 		defaults : {
-			interactions: {
-				mouseWheelZoom: true
-			},
 			controls: {
-				zoom:{
-					position: 'bottomright'
-				},
+				zoom:true,
 				rotate: true,
 				zoomtoextent: true,
 				zoomslider: true,
 				scaleline: true,
 				attribution: false
-			}			
+			}
 		},
-		
-		wms: {
+		wms: {			
 			visible: true,
-			opacity: 0.5,
+			opacity: 1,
 			source: {
-				type: 'ImageWMS',
-				url: 'http://minerva.planetek.it:8080/geoserver/cite/wms',
-				params: { 					 
-					LAYERS: 'point',
-					VERSION: '1.1.0',
-					STYLES: 'Heatmap'
+				type: 'TileWMS',
+				url: 'http://morgana.planetek.it:8080/geoserver/pkt284/wms',
+				params: {
+					LAYERS: 'PS_density_raster_10m_colorV2',
+					VERSION: '1.3.0',
+					CQL_FILTER: null
 				},
 				serverType: 'geoserver'								
+			}
+		},
+		
+		bkg: {
+			source: {
+				type: 'OSM'
 			}
 		},
 		view: {
@@ -114,4 +144,21 @@ angular.module('workspacePilotApp')
 			{ name: 'attribution', active: false}
 		]
     });
+	
+	$scope.$watch("center.zoom", function(zoom) {
+		var level = (15-(20-2*Math.floor(zoom/2)));
+		console.log(level);
+		if(level<=12) {
+			level = level<6?6:level;
+			$scope.wms.source.params.LAYERS="PS_density_raster_10m_colorV2";
+			$scope.wms.source.params.STYLES='';
+		} else {
+			$scope.wms.source.params.LAYERS="ps_limit";
+			$scope.wms.source.params.STYLES="ps_limit_style";
+		}
+	});
+	$scope.$watch("selectedDatatsets",function(dataset) {
+		if(dataset)
+			$scope.wms.source.params.CQL_FILTER="dataset_id="+dataset;
+	});
   }]);
