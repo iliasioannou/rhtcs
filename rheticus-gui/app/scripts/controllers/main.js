79a375b47b4482c6a33509db958e7fdc93f6fba4
marco fago <fago@planetek.it>
2015-10-21 11:10:33 +0200

    Working graphs

--- rheticus-gui/app/scripts/controllers/main.js
+++ rheticus-gui/app/scripts/controllers/main.js
@@ -131,20 +131,21 @@ angular.module('workspacePilotApp')
                     }
                 }
             }
-        });        
+        });
     }])
-    .controller('MainCtrl', ['$scope', 'olData', function ($scope, olData) {
-        angular.extend($scope,{
+    .controller('MainCtrl', ['$rootScope', '$scope', '$http', 'olData', function ($rootScope, $scope, $http, olData) {
+        angular.extend($scope, {
             legends: [
                 'http://morgana.planetek.it:8080/geoserver/pkt284/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=pkt284:ps_limit&TRANSPARENT=true&LEGEND_OPTIONS=fontColor:0xffffff;fontAntiAliasing:true',
                 'http://morgana.planetek.it:8080/geoserver/pkt284/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=pkt284:PS_heatmap_100m&TRANSPARENT=true&LEGEND_OPTIONS=fontColor:0xffffff;fontAntiAliasing:true'
             ]
         });
         angular.extend($scope, {
+            rc: {},
             center: {
                 lon: 11.13,
                 lat: 46.05,
-                zoom: 14
+                zoom: 19
             },
             defaults: {
                 controls: {
@@ -156,6 +157,19 @@ angular.module('workspacePilotApp')
                     attribution: false
                 }
             },
+            wms_basemap: {
+                visible: true,
+                opacity: 1,
+                source: {
+                    type: 'TileWMS',
+                    url: 'http://morgana.planetek.it:8080/geoserver/pkt284/wms',
+                    params: {
+                        LAYERS: 'rv1',
+                        VERSION: '1.3.0'
+                    }
+                }
+
+            },
             wms_raster: {
                 visible: true,
                 opacity: 0.8,
@@ -164,7 +178,7 @@ angular.module('workspacePilotApp')
                     url: 'http://morgana.planetek.it:8080/geoserver/pkt284/wms',
                     params: {
                         LAYERS: 'PS_heatmap_100m',
-                        VERSION: '1.1.0',
+                        VERSION: '1.3.0',
                         STYLES: 'PS_heatmap_100m_style',
                         minScale: 10000
                     },
@@ -172,7 +186,6 @@ angular.module('workspacePilotApp')
                 }
 
             },
-
             wms_vector: {
                 visible: true,
                 opacity: 0.8,
@@ -181,10 +194,21 @@ angular.module('workspacePilotApp')
                     url: 'http://morgana.planetek.it:8080/geoserver/pkt284/wms',
                     params: {
                         LAYERS: 'ps_limit',
-                        VERSION: '1.1.0',
+                        VERSION: '1.3.0',
                         STYLES: 'ps_limit_style',
-                        maxScale: 10000,
-                        minScale: 1000
+                        maxScale: 10000
+                    },
+                    serverType: 'geoserver'
+                }
+            },
+            wms_vector_features: {
+                visible: false,
+                source: {
+                    type: 'TileWMS',
+                    url: 'http://morgana.planetek.it:8080/geoserver/pkt284/wms',
+                    params: {
+                        LAYERS: 'ps',
+                        VERSION: '1.3.0'
                     },
                     serverType: 'geoserver'
                 }
@@ -229,6 +253,7 @@ angular.module('workspacePilotApp')
                 chart: {
                     type: 'lineChart',
                     height: 250,
+                    width: 300,
                     margin: {
                         top: 20,
                         right: 20,
@@ -291,128 +316,69 @@ angular.module('workspacePilotApp')
                     }
                 }
             },
-
-            data: function sinAndCos() {
-                var sin = [],
-                    sin2 = [],
-                    cos = [];
-
-                //Data is represented as an array of {x,y} pairs.
-                for (var i = 0; i < 100; i++) {
-                    sin.push({
-                        x: i,
-                        y: Math.sin(i / 10)
-                    });
-                    sin2.push({
-                        x: i,
-                        y: i % 10 === 5 ? null : Math.sin(i / 10) * 0.25 + 0.5
-                    });
-                    cos.push({
-                        x: i,
-                        y: 0.5 * Math.cos(i / 10 + 2) + Math.random() / 10
-                    });
-                }
-
-                //Line chart data should be sent as an array of series objects.
-                return [
-                    {
-                        values: sin, //values - represents the array of {x,y} data points
-                        key: 'Sine Wave', //key  - the name of the series.
-                        color: '#ff7f0e' //color - optional: choose your own line color.
-                },
-                    {
-                        values: cos,
-                        key: 'Cosine Wave',
-                        color: '#2ca02c'
-                },
-                    {
-                        values: sin2,
-                        key: 'Another sine wave',
-                        color: '#7777ff',
-                        area: true //area - set to true if you want this line to turn into a filled area chart.
-                },
-
-                    {
-                        values: [{
-                            x: 0,
-                            y: 0
-                        }, {
-                            x: 1,
-                            y: 1
-                        }],
-                        key: 'Pippo',
-                        color: '#7777ff',
-                        area: true //area - set to true if you want this line to turn into a filled area chart.
-                }
-            ];
-            }
-
+            data: []
         });
         $scope.$watch("selectedDatatsets", function (dataset) {
             if (dataset)
                 $scope.wms.source.params.CQL_FILTER = "dataset_id=" + dataset;
         });
 
-        $scope.addWFS = function () {
-
-            var loadFeatures = function (response) {
-                var formatWFS = new ol.format.WFS();
-                sourceVector.addFeatures(formatWFS.readFeatures(response));
-            };
-
-            var sourceVector = new ol.source.Vector({
-                loader: function (extent) {
-                    console.log(extent);
-                    $.ajax('http://morgana.planetek.it:8080/geoserver/pkt284/wms', {
-                        type: 'GET',
-                        data: {
-                            service: 'WFS',
-                            version: '2.0.0',
-                            request: 'GetFeature',
-                            typename: 'pkt284:ps',
-                            srsname: 'EPSG:3857',
-                            bbox: extent.join(',') + ',EPSG:3857'
-                        },
-                    }).done(loadFeatures);
-                },
-                strategy: ol.loadingstrategy.tile(new ol.tilegrid.createXYZ({
-                    maxZoom: 19
-                })),
-            });
-
-            var layerVector = new ol.layer.Vector({
-                source: sourceVector,
-                style: new ol.style.Style({
-                    stroke: new ol.style.Stroke({
-                        color: 'rgba(0, 0, 255, 1.0)',
-                        width: 2
-                    })
-                })
-            });
-
-            olData.getMap().then(function (map) {
-                map.on('singleclick', function (evt) {
-                    console.log("Map clicked");
-                });
-            });
-        };
-
         olData.getMap().then(function (map) {
             map.on('singleclick', function (evt) {
-                console.log("Map clicked");
+                var viewResolution = map.getView().getResolution();
+                var wmsSource = new ol.source.TileWMS($scope.wms_vector_features.source);
+                var url = wmsSource.getGetFeatureInfoUrl(
+                    evt.coordinate, viewResolution, 'EPSG:3857', {
+                        'INFO_FORMAT': 'application/json'
+                    });
+
+
+                var json = [];
+                $http.get(url).success(function (response) {
+                    //For IE :: creating "startsWith" method for String Class
+                    if (typeof String.prototype.startsWith != 'function') {
+                        String.prototype.startsWith = function (str) {
+                            return this.slice(0, str.length) == str;
+                        };
+                    }
+                    //Data is represented as an array of {x,y} pairs.                     
+                    if (response.features.length > 0) {
+                        $scope.show_panel = true;
+                        for (var key in response.features[0].properties) {
+                            if (key.startsWith("dl")) {
+                                json.push({
+                                    //                                    x: d3.time.format('%x')(new Date(key.replace("dl", ""))),
+                                    x: key.replace("dl", ""),
+                                    y: response.features[0].properties[key]
+                                });
+                            }
+                        }
+                       
+                    }else{
+                        $scope.show_panel = false;
+                    }
+                    //Line chart data should be sent as an array of series objects.                
+                    angular.extend($scope.data, [
+                        {
+                            values: json, //values - represents the array of {x,y} data points
+                            key: 'PS', //key  - the name of the series.
+                            color: '#ff7f0e' //color - optional: choose your own line color.
+                        }
+                    ]);
+                    $scope.rc.api.update();
+                });
             });
         });
-        //$scope.addWFS();
-
     }])
-    .controller('DetailController', ['$scope', function ($scope) {
-        angular.extend($scope, {
-            show: true,
-            switch: function () {
-                $scope.show = !$scope.show;
-            },
-            isVisible: function () {
-                return $scope.show;
-            }
-        });
+
+.controller('DetailController', ['$scope', function ($scope) {
+    angular.extend($scope, {
+        show: true,
+        switch: function () {
+            $scope.show = !$scope.show;
+        },
+        isVisible: function () {
+            return $scope.show;
+        }
+    });
     }]);
