c441027dc6e091156abdeeb138009174ec8abf01
lunanova <lunanova@planetek.it>
2015-11-12 18:14:16 +0100

    Refactoring and added configuration files

---
+++ rheticus-gui/app/scripts/components/about/about-controller.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rheticus.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rheticus
+ */
+angular.module('rheticus')
+    .controller('AboutCtrl', function () {
+    });
