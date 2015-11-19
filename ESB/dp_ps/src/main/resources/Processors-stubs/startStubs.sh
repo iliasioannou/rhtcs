d117886a9739447b2742aadda7db84aea13588d5
lunanova <lunanova@planetek.it>
2015-11-19 12:11:34 +0100

    test

--- ESB/dp_ps/src/main/resources/Processors-stubs/startStubs.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-echo Starting Processors stubs...
-python SuperMaster/supermaster-rest.py 2>&1 | tee  sm.log &
-python S1TBX/s1tbx-rest.py 2>&1 | tee s1tbx.log &
-python PSinSAR/psinsar-rest.py 2>&1 | tee psinsar.log &
-echo DONE
