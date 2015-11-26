08a3941d8bae0ee08fcd95a853348f86a0deae88
sportelli <sportelli@planetek.it>
2015-11-26 16:19:51 +0100

    Add IFFI project layer

--- rheticus-gui/app/scripts/components/legend/legend-controller.js
+++ rheticus-gui/app/scripts/components/legend/legend-controller.js
@@ -13,11 +13,23 @@ angular.module('rheticus')
 	angular.extend($scope,{
 		"legendUrl" : configuration.legends.density.url,
 		"legendTitle" : configuration.legends.density.title,
-		"legendUom" : configuration.legends.density.uom
+		"legendUom" : configuration.legends.density.uom,
+		
+		"legendUrlFrane" : configuration.legends.iffi.url,
+		"legendTitleFrane" : configuration.legends.iffi.title,
+		"legendUomFrane" : configuration.legends.iffi.uom,
 	});
 	$scope.$watch("center.zoom", function (zoom) { //legend visibility Heatmap/PS
 		$scope.legendUrl = configuration.legends[$scope.showDetails() ? "ps" : "density"].url;
 		$scope.legendTitle = configuration.legends[$scope.showDetails() ? "ps" : "density"].title;
 		$scope.legendUom = configuration.legends[$scope.showDetails() ? "ps" : "density"].uom;
+	});
+	
+	$scope.$watch("overlays", function () { //legend visibility frane
+		if ($scope.showLegends("iffi")){
+			$scope.legendUrlFrane = configuration.legends["iffi"].url;
+			$scope.legendTitleFrane = configuration.legends["iffi"].title;
+			$scope.legendUomFrane = configuration.legends["iffi"].uom;
+		}
 	});
 }]);
