1e8703895276977522015416e1cf31523d8f8c39
sportelli <sportelli@planetek.it>
2015-12-03 12:31:53 +0100

    refactored geocoder-component

--- rheticus-gui/app/scripts/components/geocoder/geocoder-controller.js
+++ rheticus-gui/app/scripts/components/geocoder/geocoder-controller.js
@@ -9,51 +9,26 @@
  */
 
 angular.module('rheticus')
-	.controller('GeocoderCtrl',['$scope','$http','olData', function ($scope,$http,olData) {
-		angular.extend($scope,{
-			//"geoPopup" : "scripts/components/geocoder/geo-popup-view.html",			
-			"geoFocus": {
-				"lat": "",
-				"lng": "",
-				"zoom": 12
-			},
-			"searching" : false,
-			"results":{}
-		});
-		this.urlQuery= "http://nominatim.openstreetmap.org/search?q=";
-		this.urlParams="&limit=5&format=json";
-		this.location="";
-		this.results={};
-		this.zoom= 10;
+	.controller('GeocoderCtrl',['$rootScope','$scope','$http','configuration', function ($rootScope,$scope,$http,configuration) {
+		//controller variables
+		this.searching = false;
+		this.results = {};
+		this.location = "";
 		this.searchLocation = function(){
 			this.location = this.location.replace('/[^a-zA-Z0-9]/g','+');
-			//console.log(this.location);
-			$http.get(this.urlQuery+this.location+this.urlParams+"").success(function (response) {
-				$scope.results= response;
-			});
+			var that = this;
+			$http.get(configuration.geocoder.url+this.location+configuration.geocoder.params)
+				.success(function (response) {
+					that.results = response;
+				});
 		};
 		
 		this.getLocation = function(index){			
-			var jsonLocation= $scope.results[index];			
-			var extent = [parseFloat(jsonLocation.boundingbox[2]),
-					parseFloat(jsonLocation.boundingbox[0]),
-					parseFloat(jsonLocation.boundingbox[3]),
-					parseFloat(jsonLocation.boundingbox[1])
-					];
-			console.log(extent);
-			olData.getMap().then(function(map) {
-				
-				map.getView().fit(extent, map.getSize());
-				$scope.center.bounds = extent;
-				$scope.center.lon = parseFloat(jsonLocation.lon);
-                $scope.center.lat = parseFloat(jsonLocation.lat);				
-                $scope.center.zoom = 10;
-				//$scope.center.zoom = map.getZoomForExtent(extent);
-			});
-				
-			$scope.searching=false;			//hide search after delection
-			$scope.results= {};
-			this.location="";
-			console.log($scope.center);			
+			var jsonLocation = this.results[index];			
+			$rootScope.center.lon = parseFloat(jsonLocation.lon);
+            $rootScope.center.lat = parseFloat(jsonLocation.lat);
+			this.searching = false;			//hide search after delection
+			this.results = {};
+			this.location = "";
 		};
 	}]);
