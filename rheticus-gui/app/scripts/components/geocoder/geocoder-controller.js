01e469c02fc0e3a2ab744c0ac89d91fe12696fa0
sportelli <sportelli@planetek.it>
2015-12-03 11:45:27 +0100

    Fixed geocoder style

--- rheticus-gui/app/scripts/components/geocoder/geocoder-controller.js
+++ rheticus-gui/app/scripts/components/geocoder/geocoder-controller.js
@@ -9,15 +9,51 @@
  */
 
 angular.module('rheticus')
-	.controller('GeocoderCtrl',['$scope', function ($scope) {
+	.controller('GeocoderCtrl',['$scope','$http','olData', function ($scope,$http,olData) {
 		angular.extend($scope,{
-			/*
-			"geoPopup" : "scripts/components/geocoder/geocoder-popup-view.html",
+			//"geoPopup" : "scripts/components/geocoder/geo-popup-view.html",			
 			"geoFocus": {
 				"lat": "",
 				"lng": "",
 				"zoom": 12
-			}
-			*/
+			},
+			"searching" : false,
+			"results":{}
 		});
+		this.urlQuery= "http://nominatim.openstreetmap.org/search?q=";
+		this.urlParams="&limit=5&format=json";
+		this.location="";
+		this.results={};
+		this.zoom= 10;
+		this.searchLocation = function(){
+			this.location = this.location.replace('/[^a-zA-Z0-9]/g','+');
+			//console.log(this.location);
+			$http.get(this.urlQuery+this.location+this.urlParams+"").success(function (response) {
+				$scope.results= response;
+			});
+		};
+		
+		this.getLocation = function(index){			
+			var jsonLocation= $scope.results[index];			
+			var extent = [parseFloat(jsonLocation.boundingbox[2]),
+					parseFloat(jsonLocation.boundingbox[0]),
+					parseFloat(jsonLocation.boundingbox[3]),
+					parseFloat(jsonLocation.boundingbox[1])
+					];
+			console.log(extent);
+			olData.getMap().then(function(map) {
+				
+				map.getView().fit(extent, map.getSize());
+				$scope.center.bounds = extent;
+				$scope.center.lon = parseFloat(jsonLocation.lon);
+                $scope.center.lat = parseFloat(jsonLocation.lat);				
+                $scope.center.zoom = 10;
+				//$scope.center.zoom = map.getZoomForExtent(extent);
+			});
+				
+			$scope.searching=false;			//hide search after delection
+			$scope.results= {};
+			this.location="";
+			console.log($scope.center);			
+		};
 	}]);
