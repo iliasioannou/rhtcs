9addb9d71207f04783442d69e8d1bd280f419816
sportelli <sportelli@planetek.it>
2015-12-04 08:56:08 +0100

    fixed credits

---
+++ rheticus-gui/app/scripts/components/credits/credits-controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rheticus.controller:CreditsController
+ * @description
+ * # CreditsController
+ * Controller of the rheticus
+ */
+
+angular.module('rheticus')
+	.controller('CreditsController', ['configuration',"$scope", function (configuration,$scope) {
+		this.show = false;
+	
+	}]);
